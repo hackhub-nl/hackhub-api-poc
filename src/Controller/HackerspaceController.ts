a169f8c2896b966cece2d4cfa2eec42bac90b694
ivanab <ivana@opticaltone.com>
2024-03-18 18:13:32 +0100

    Added operations to HackerspaceController class

--- src/Controller/HackerspaceController.ts
+++ src/Controller/HackerspaceController.ts
@@ -22,4 +22,56 @@ class HackerspaceController {
       });
     }
   }
+
+  async delete(req: Request, res: Response) {
+    try {
+        let id = parseInt(req.params["id"]);
+        await new HackerspaceRepo().delete(id);
+
+      res.status(200).json({
+        status: "Ok!",
+        message: "Successfully deleted hackerspace!",
+      });
+    } catch (err) {
+      res.status(500).json({
+        status: "Internal Server Error!",
+        message: "Internal Server Error!",
+      });
+    }
+  }
+
+  async findAll(req:Request, res:Response) {
+    try {
+      const hspace = await new HackerspaceRepo().retrieveAll()
+
+      res.status(200).json({
+        status: "Ok!",
+        message: "Successfully fetched all hackerspaces!",
+        data: hspace
+      });
+    } catch (err) {
+      res.status(500).json({
+        status: "Internal Server Error!",
+        message: "Internal Server Error!",
+      });
+    }
+  }
+
+  async findById(req:Request,res:Response) {
+    try {
+        let id = parseInt(req.params["id"]);
+        const hspace = await new HackerspaceRepo().retrieveById(id)
+  
+        res.status(200).json({
+          status: "Ok!",
+          message: "Successfully fetched hackerspace by id!",
+          data: hspace
+        });
+      } catch (err) {
+        res.status(500).json({
+          status: "Internal Server Error!",
+          message: "Internal Server Error!",
+        });
+      }
+  }
 }
