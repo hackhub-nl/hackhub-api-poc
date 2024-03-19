a1c44ae72737741bae0662403431d9420e4f44c7
ivanab <ivana@opticaltone.com>
2024-03-19 12:47:41 +0100

    Created Event model

---
+++ src/model/Event.ts
@@ -0,0 +1,31 @@
+import { Model, Table, Column, DataType } from "sequelize-typescript";
+
+@Table({
+  tableName: Event.EVENT_TABLE_NAME,
+})
+export class Event extends Model {
+  public static EVENT_TABLE_NAME = "events" as string;
+  public static EVENT_ID = "id" as string;
+  public static EVENT_NAME = "name" as string;
+  public static EVENT_DESCRIPTION = "description" as string;
+
+  @Column({
+    type: DataType.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    field: Event.EVENT_ID,
+  })
+  id!: number;
+
+  @Column({
+    type: DataType.STRING(100),
+    field: Event.EVENT_NAME,
+  })
+  name!: string;
+
+  @Column({
+    type: DataType.STRING(500),
+    field: Event.EVENT_DESCRIPTION,
+  })
+  city!: string;
+}
