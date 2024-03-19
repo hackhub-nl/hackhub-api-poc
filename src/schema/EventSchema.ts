feabc6a4dd824e65b45d9dd709ab48d621fa394c
ivanab <ivana@opticaltone.com>
2024-03-19 14:49:40 +0100

    Created EventSchema

---
+++ src/schema/EventSchema.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+
+export const createEventSchema = z.object({
+  body: z.object({
+    name: z
+      .string()
+      .min(0, { message: "Name must contain at least one character!" }),
+    description: z.string().min(1, {
+      message: "Description must contain at least two characters!",
+    }),
+  }),
+});
+
+export const updateHackerspaceSchema = z.object({
+  params: z.object({ id: z.string() }),
+  body: z
+    .object({
+      name: z
+        .string()
+        .min(0, { message: "Name must contain at least one character!" }),
+      description: z.string().min(1, {
+        message: "Description must contain at least two characters!",
+      }),
+    })
+    .partial(),
+});
