d11d741f781424cd26973da74d072df333af1bf9
ivanab <ivana@opticaltone.com>
2024-03-27 16:49:04 +0100

    Modified login method; Implemented register method;

--- src/controller/AuthenticationController.ts
+++ src/controller/AuthenticationController.ts
@@ -6,15 +6,19 @@ export class AuthenticationController {
     try {
       const { email, password } = req.body;
       const token = await new AuthenticationService().login(email, password);
+
       if (token === "") {
         return res.status(400).json({
           status: "Bad request!",
           message: "Wrong email or password!",
         });
       }
-      return res.status(500).json({
+      const res_token = { type: "Bearer", token: token };
+
+      return res.status(200).json({
         status: "Ok!",
         message: "Successfully logged in!",
+        result: res_token,
       });
     } catch (error) {
       return res.status(500).json({
@@ -23,5 +27,25 @@ export class AuthenticationController {
       });
     }
   }
-  async register(req: Request, res: Response) {}
+  async register(req: Request, res: Response) {
+    try {
+      const { name, username, email, password } = req.body;
+      await new AuthenticationService().register(
+        email,
+        password,
+        name,
+        username
+      );
+
+      return res.status(200).json({
+        status: "Ok!",
+        message: "Successfully registered!",
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: "Internal server error!",
+        message: "Internal server error!",
+      });
+    }
+  }
 }
