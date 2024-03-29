eb8f9fff7dd2ac71eba1c5181d8506ce1c1b5764
ivanab <ivana@opticaltone.com>
2024-03-29 14:07:58 +0100

    Added useful npm packages

--- src/config/default.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  serverPort: process.env.SERVER_PORT,
-  clientUri: process.env.CLIENT_URL,
-  jwtSecretKey: process.env.JWT_SECRET_KEY,
-  jwtExpiresIn: process.env.JWT_EXPIRES_IN
-};
