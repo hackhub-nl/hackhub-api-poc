711ffefef682f653af9fd38a7a38f9228b072d32
ivanab <ivana@opticaltone.com>
2024-03-29 20:23:50 +0100

    Modified user.schema

--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -1,14 +1,14 @@
 import UserController from "../controllers/user.controller";
 import BaseRoutes from "./base/base.router";
 import validateResource from "../middleware/validateResource";
-import { createUserSchema } from "../schema/user.schema";
+import { registerUserSchema } from "../schema/user.schema";
 
 class UserRoutes extends BaseRoutes {
   routes(): void {
     this.router.post("/login", UserController.login);
     this.router.post(
       "/register",
-      validateResource(createUserSchema),
+      validateResource(registerUserSchema),
       UserController.register
     );
   }
--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -1,21 +1,19 @@
-import { object, string } from "zod";
+import { z } from "zod";
 
-export const createUserSchema = object({
-  body: object({
-    name: string({
+export const registerUserSchema = z.object({
+  body: z.object({
+    name: z.string({
       required_error: "Name is required",
     }),
-    password: string({
-      required_error: "Password is required",
-    }).min(6, "Password too short - should be 6 chars minimum"),
-    passwordConfirmation: string({
-      required_error: "Password confirmed is required",
-    }),
-    email: string({
-      required_error: "Email is required",
-    }).email("Not a valid email"),
-  }).refine((data) => data.password === data.passwordConfirmation, {
-    message: "Passwords do not match",
-    path: ["passwordConfirmation"],
+    password: z
+      .string({
+        required_error: "Password is required",
+      })
+      .min(6, "Password too short - should be 6 chars minimum"),
+    email: z
+      .string({
+        required_error: "Email is required",
+      })
+      .email("Not a valid email"),
   }),
 });
