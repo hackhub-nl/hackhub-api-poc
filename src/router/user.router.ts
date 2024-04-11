b5ad799acf285f416df9debf15e6cd17c6339083
ivanab <ivana@opticaltone.com>
2024-04-11 19:37:41 +0200

    Used register user handler

--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -1,15 +1,15 @@
-import UserController from "../controllers/user.controller";
+import { registerUserHandler } from "../controllers/user.controller";
 import BaseRoutes from "./base/base.router";
 import validateResource from "../middleware/validateResource";
 import { registerUserSchema } from "../schema/user.schema";
 
 class UserRoutes extends BaseRoutes {
   routes(): void {
-    this.router.post("/login", UserController.login);
+    //this.router.post("/login", UserController.login);
     this.router.post(
       "/register",
       validateResource(registerUserSchema),
-      UserController.register
+      registerUserHandler
     );
   }
 }
