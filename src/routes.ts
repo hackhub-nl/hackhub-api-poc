98f9a003e8cda2fa80f3469f4239634d73e998d5
ivanab <ivana@opticaltone.com>
2024-04-11 22:34:02 +0200

    Added post login session route

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,8 @@ import { Express, Request, Response } from "express";
 import { registerUserSchema } from "./schema/user.schema";
 import validateResource from "./middleware/validateResource";
 import { registerUserHandler } from "./controllers/user.controller";
+import { loginSessionSchema } from "./schema/session.schema";
+import { loginUserSessionHandler } from "./controllers/session.controller";
 
 function routes(app: Express) {
   app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
@@ -10,6 +12,12 @@ function routes(app: Express) {
     "/api/users",
     validateResource(registerUserSchema),
     registerUserHandler
+  );
+
+  app.post(
+    "/api/sessions",
+    validateResource(loginSessionSchema),
+    loginUserSessionHandler
   );
 }
 
