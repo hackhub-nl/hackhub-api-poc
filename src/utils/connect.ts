30c521ddd64c1963b93857902cf5cc89fdbe7930
ivanab <ivana@opticaltone.com>
2024-04-19 18:29:18 +0200

    Modified connect and delteted commented out code

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -6,7 +6,7 @@ import { Event } from "../models/event.model";
 import { User } from "../models/user.model";
 import { Session } from "../models/session.model";
 
-function connect() {
+async function connect() {
   const sequelize = new Sequelize({
     database: config.get<string>("postgresDB"),
     host: config.get<string>("postgresHost"),
@@ -17,46 +17,13 @@ function connect() {
     models: [Hackerspace, Event, User, Session],
   });
 
-  return sequelize
-    .authenticate()
-    .then(() => {
-      logger.info("DB connected");
-    })
-    .catch((error) => {
-      logger.error("Could not connect to DB");
-      process.exit(1);
-    });
+  try {
+    await sequelize.authenticate();
+    logger.info("DB connected");
+  } catch (error) {
+    logger.error("Could not connect to DB");
+    process.exit(1);
+  }
 }
 
 export default connect;
-
-// class Database {
-//   public sequelize: Sequelize | undefined;
-
-//   constructor() {
-//     this.connectToPostgreSQL();
-//   }
-
-//   private async connectToPostgreSQL() {
-//     this.sequelize = new Sequelize({
-//       database: config.get<string>("postgresDB"),
-//       host: config.get<string>("postgresHost"),
-//       port: config.get<number>("postgresPort"),
-//       username: config.get<string>("postgresUser"),
-//       password: config.get<string>("postgresPassword"),
-//       dialect: "postgres",
-//       models: [Hackerspace, Event, User, Session],
-//     });
-
-//     await this.sequelize
-//       .authenticate()
-//       .then(() => {
-//         logger.info("Connected to the database");
-//       })
-//       .catch((err) => {
-//         logger.error("Unable to conned to the database", err);
-//       });
-//   }
-// }
-
-// export default Database;
