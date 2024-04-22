9fc077e55507b488e56ef602bdb7a3cd8c58b862
ivanab <ivana@opticaltone.com>
2024-04-22 15:39:35 +0200

    Added validation rules to registerUserSchema

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -1,28 +1,39 @@
 import { TypeOf, z } from "zod";
 
 export const registerUserSchema = z.object({
-  body: z.object({
-    email: z
-      .string({
-        required_error: "Email is required",
-      })
-      .email("Not a valid email"),
-    name: z.string({
-      required_error: "Name is required",
+  body: z
+    .object({
+      email: z
+        .string({
+          required_error: "Email is required",
+        })
+        .email("Not a valid email"),
+      name: z
+        .string({
+          required_error: "Name is required",
+        })
+        .email("Not valid email"),
+      password: z
+        .string({
+          required_error: "Password is required",
+        })
+        .min(6, "Password too short - should be 6 chars minimum"),
+      passwordConfirmation: z.string({
+        required_error: "Password confirmation is required",
+      }),
+      createdAt: z.date({
+        required_error: "Crated at time is required",
+      }),
+      updatedAt: z.date({
+        required_error: "Updated at time is required",
+      }),
+    })
+    .refine((data) => data.password === data.passwordConfirmation, {
+      message: "Passwords do not match",
+      path: ["passwordConfirmation"],
     }),
-    password: z
-      .string({
-        required_error: "Password is required",
-      })
-      .min(6, "Password too short - should be 6 chars minimum"),
-    createdAt: z.date({
-      required_error: "Crated at time is required",
-    }),
-    updatedAt: z.date({
-      required_error: "Updated at time is required",
-    }),
-  }),
 });
+
 export type RegisterUserInput = Omit<
   TypeOf<typeof registerUserSchema>,
   "body.passwordConfirmation"
