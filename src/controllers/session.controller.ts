a14045fe0fc433450df5cebdc0ad02f93ddb5617
ivanab <ivana@opticaltone.com>
2024-04-26 17:11:34 +0200

    Removed comments for obvious operations

--- src/controllers/session.controller.ts
+++ src/controllers/session.controller.ts
@@ -9,29 +9,24 @@ import { signJwt } from "../utils/jwt.utils";
 import config from "config";
 
 export async function loginUserSessionHandler(req: Request, res: Response) {
-  // Validate the user's password
   const user = await validatePassword(req.body);
 
   if (!user) {
     return res.status(401).send("Invalid email or password");
   }
 
-  // create a session
   const session = await loginSession(user.id, req.get("user-agent") || "");
 
-  // create an access token
   const accessToken = signJwt(
     { ...user, session: session.id },
-    { expiresIn: config.get("accessTokenTtl") } // 15 minutes,
+    { expiresIn: config.get("accessTokenTtl") }
   );
 
-  // create a refresh token
   const refreshToken = signJwt(
     { ...user, session: session.id },
-    { expiresIn: config.get("refreshTokenTtl") } // 15 minutes
+    { expiresIn: config.get("refreshTokenTtl") }
   );
 
-  // return access & refresh tokens
   return res.send({ accessToken, refreshToken });
 }
 
