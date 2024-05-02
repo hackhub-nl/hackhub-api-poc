258cd934d1a8923afa8d478fa14c790f6f6eae79
ivanab <ivana@opticaltone.com>
2024-05-02 17:44:14 +0200

    Commented out not used code

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,23 +1,23 @@
-import config from "config";
-import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
+// import config from "config";
+// import { Request, Response, NextFunction } from "express";
+// import jwt from "jsonwebtoken";
 
-export const auth = (req: Request, res: Response, next: NextFunction): any => {
-  if (!req.headers.authorization) {
-    return res.status(401).send("No token!");
-  }
+// export const auth = (req: Request, res: Response, next: NextFunction): any => {
+//   if (!req.headers.authorization) {
+//     return res.status(401).send("No token!");
+//   }
 
-  let secretKey: string = config.get<string>("jwtSecretKey") || "secret";
-  const token: string = req.headers.authorization.split(" ")[1];
+//   let secretKey: string = config.get<string>("jwtSecretKey") || "secret";
+//   const token: string = req.headers.authorization.split(" ")[1];
 
-  try {
-    const credentials: string | Object = jwt.verify(token, secretKey);
-    if (credentials) {
-      req.app.locals.credentials = credentials;
-      return next();
-    }
-    return res.send("Invalid token!");
-  } catch (error) {
-    return res.send(error);
-  }
-};
+//   try {
+//     const credentials: string | Object = jwt.verify(token, secretKey);
+//     if (credentials) {
+//       req.app.locals.credentials = credentials;
+//       return next();
+//     }
+//     return res.send("Invalid token!");
+//   } catch (error) {
+//     return res.send(error);
+//   }
+// };
