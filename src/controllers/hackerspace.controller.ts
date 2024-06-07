923c2d1d1d2f7928fa9cd9a76887ef9b7539d792
ivanab <ivana@opticaltone.com>
2024-06-07 15:29:46 +0200

    Refactoring hackerspace.controller

--- src/controllers/hackerspace.controller.ts
+++ src/controllers/hackerspace.controller.ts
@@ -1,101 +1,119 @@
 import { Request, Response } from "express";
-import { Hackerspace } from "../models/hackerspace.model";
-import { HackerspaceRepo } from "../repositories/hackerspace.repo";
 
-class HackerspaceController {
-  async create(req: Request, res: Response) {
-    try {
-      const hspace = new Hackerspace();
-      hspace.name = req.body.name;
-      hspace.city = req.body.city;
-
-      await new HackerspaceRepo().save(hspace);
-
-      res.status(201).json({
-        status: "Created!",
-        message: "Successfully created hackerspace!",
-      });
-    } catch (err) {
-      res.status(500).json({
-        status: "Internal Server Error!",
-        message: "Internal Server Error!",
-      });
-    }
-  }
-
-  async delete(req: Request, res: Response) {
-    try {
-      let id = parseInt(req.params["id"]);
-      await new HackerspaceRepo().delete(id);
-
-      res.status(200).json({
-        status: "Ok!",
-        message: "Successfully deleted hackerspace!",
-      });
-    } catch (err) {
-      res.status(500).json({
-        status: "Internal Server Error!",
-        message: "Internal Server Error!",
-      });
-    }
-  }
-
-  async findById(req: Request, res: Response) {
-    try {
-      let id = parseInt(req.params["id"]);
-      const hspace = await new HackerspaceRepo().retrieveById(id);
-
-      res.status(200).json({
-        status: "Ok!",
-        message: "Successfully fetched hackerspace by id!",
-        data: hspace,
-      });
-    } catch (err) {
-      res.status(500).json({
-        status: "Internal Server Error!",
-        message: "Internal Server Error!",
-      });
-    }
-  }
-
-  async findAll(req: Request, res: Response) {
-    try {
-      const hspace = await new HackerspaceRepo().retrieveAll();
-
-      res.status(200).json({
-        status: "Ok!",
-        message: "Successfully fetched all hackerspaces!",
-        data: hspace,
-      });
-    } catch (err) {
-      res.status(500).json({
-        status: "Internal Server Error!",
-        message: "Internal Server Error!",
-      });
-    }
-  }
-
-  async update(req: Request, res: Response) {
-    try {
-      let id = parseInt(req.params["id"]);
-      const hspace = new Hackerspace();
-      hspace.id = id;
-      hspace.name = req.body.name;
-      hspace.city = req.body.city;
-
-      await new HackerspaceRepo().update(hspace);
-
-      res.status(200).json({
-        status: "Ok!",
-        message: "Successfully updated hackerspace!",
-      });
-    } catch (err) {
-      res.status(500).json({
-        status: "Internal Server Error!",
-        message: "Internal Server Error!",
-      });
-    }
-  }
+export async function createHackerspaceHandler(req: Request, res: Response) {
+  
 }
 
-export default new HackerspaceController();
+export async function updateHackerspaceHandler(req: Request, res: Response) {
+  
+}
+
+export async function getHackerspaceHandler(req: Request, res: Response) {
+  
+}
+
+export async function deleteHackerspaceHandler(req: Request, res: Response) {
+  
+}
+
+// import { Request, Response } from "express";
+// import { Hackerspace } from "../models/hackerspace.model";
+// import { HackerspaceRepo } from "../repositories/hackerspace.repo";
+
+// class HackerspaceController {
+//   async create(req: Request, res: Response) {
+//     try {
+//       const hspace = new Hackerspace();
+//       hspace.name = req.body.name;
+//       hspace.city = req.body.city;
+
+//       await new HackerspaceRepo().save(hspace);
+
+//       res.status(201).json({
+//         status: "Created!",
+//         message: "Successfully created hackerspace!",
+//       });
+//     } catch (err) {
+//       res.status(500).json({
+//         status: "Internal Server Error!",
+//         message: "Internal Server Error!",
+//       });
+//     }
+//   }
+
+//   async delete(req: Request, res: Response) {
+//     try {
+//       let id = parseInt(req.params["id"]);
+//       await new HackerspaceRepo().delete(id);
+
+//       res.status(200).json({
+//         status: "Ok!",
+//         message: "Successfully deleted hackerspace!",
+//       });
+//     } catch (err) {
+//       res.status(500).json({
+//         status: "Internal Server Error!",
+//         message: "Internal Server Error!",
+//       });
+//     }
+//   }
+
+//   async findById(req: Request, res: Response) {
+//     try {
+//       let id = parseInt(req.params["id"]);
+//       const hspace = await new HackerspaceRepo().retrieveById(id);
+
+//       res.status(200).json({
+//         status: "Ok!",
+//         message: "Successfully fetched hackerspace by id!",
+//         data: hspace,
+//       });
+//     } catch (err) {
+//       res.status(500).json({
+//         status: "Internal Server Error!",
+//         message: "Internal Server Error!",
+//       });
+//     }
+//   }
+
+//   async findAll(req: Request, res: Response) {
+//     try {
+//       const hspace = await new HackerspaceRepo().retrieveAll();
+
+//       res.status(200).json({
+//         status: "Ok!",
+//         message: "Successfully fetched all hackerspaces!",
+//         data: hspace,
+//       });
+//     } catch (err) {
+//       res.status(500).json({
+//         status: "Internal Server Error!",
+//         message: "Internal Server Error!",
+//       });
+//     }
+//   }
+
+//   async update(req: Request, res: Response) {
+//     try {
+//       let id = parseInt(req.params["id"]);
+//       const hspace = new Hackerspace();
+//       hspace.id = id;
+//       hspace.name = req.body.name;
+//       hspace.city = req.body.city;
+
+//       await new HackerspaceRepo().update(hspace);
+
+//       res.status(200).json({
+//         status: "Ok!",
+//         message: "Successfully updated hackerspace!",
+//       });
+//     } catch (err) {
+//       res.status(500).json({
+//         status: "Internal Server Error!",
+//         message: "Internal Server Error!",
+//       });
+//     }
+//   }
+// }
+
+// export default new HackerspaceController();
