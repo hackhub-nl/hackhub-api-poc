1d2a23b8103e1d8f17042908ef595ec1bdfbf3a5
ivanab <ivana@opticaltone.com>
2024-06-07 15:57:58 +0200

    Refactored hackerspace.schema

--- src/schema/hackerspace.schema.ts
+++ src/schema/hackerspace.schema.ts
@@ -1,34 +1,42 @@
-import { z } from "zod";
+import { object, number, string, TypeOf } from "zod";
 
-export const createHackerspaceSchema = z.object({
-  body: z.object({
-    name: z
-      .string({
-        required_error: "Name is required",
-      })
-      .min(0, { message: "Name must contain at least one character!" }),
-    city: z
-      .string({
-        required_error: "City is required",
-      })
-      .min(0, { message: "City must contain at least one character!" }),
+const payload = {
+  body: object({
+    name: string({
+      required_error: "Name is required",
+    }).min(0, { message: "Name must contain at least one character" }),
+    city: string({
+      required_error: "City is required",
+    }).min(0, { message: "City must contain at least one character" }),
   }),
+};
+
+const params = {
+  params: object({
+    id: string({
+      required_error: "Hackerspace id is required",
+    }),
+  }),
+};
+
+export const getHackerspaceSchema = object({
+  ...params,
 });
 
-export const updateHackerspaceSchema = z.object({
-  params: z.object({ id: z.string() }),
-  body: z
-    .object({
-      name: z
-        .string({
-          required_error: "Name is required",
-        })
-        .min(0, { message: "Name must contain at least one character!" }),
-      city: z
-        .string({
-          required_error: "City is required",
-        })
-        .min(0, { message: "City must contain at least one character!" }),
-    })
-    .partial(),
+export const createHackerspaceSchema = object({
+  ...payload,
 });
+
+export const updateHackerspaceSchema = object({
+  ...payload,
+  ...params,
+});
+
+export const deleteHackerspaceSchema = object({
+  ...params,
+});
+
+export type GetHackerspaceSchema = TypeOf<typeof getHackerspaceSchema>;
+export type CreateHackerspaceSchema = TypeOf<typeof createHackerspaceSchema>;
+export type UpdateHackerspaceSchema = TypeOf<typeof updateHackerspaceSchema>;
+export type DeleteHackerspaceSchema = TypeOf<typeof deleteHackerspaceSchema>;
