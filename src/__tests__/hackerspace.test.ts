1d3533ed3888b030d6715320f5118cbd0e6076cd
ivanab <ivana@opticaltone.com>
2024-06-17 19:24:49 +0200

    test: added get hackerspace route test

---
+++ src/__tests__/hackerspace.test.ts
@@ -0,0 +1,9 @@
+describe("hackerspace", () => {
+  describe("get hackerspace route", () => {
+    describe("given the hackerspace does not exist", () => {
+      it("should return a 404", () => {
+        expect(true).toBe(true);
+      });
+    });
+  });
+});
