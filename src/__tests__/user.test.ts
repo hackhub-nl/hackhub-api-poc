86510ec548feb5b9e4e1f6a0bafb8b261d31c5bf
ivanab <ivana@opticaltone.com>
2024-06-19 16:10:18 +0200

    test: given the username and password are valid, should return user payload

--- src/__tests__/user.test.ts
+++ src/__tests__/user.test.ts
@@ -1,60 +1,62 @@
-// import supertest from "supertest";
+import supertest from "supertest";
 // import createServer from "../utils/server";
-// import * as UserService from "../service/user.service";
+import * as UserService from "../service/user.service";
+import { app } from "../app";
 // import { User } from "../models/user.model";
 
 // const app = createServer();
 
-// const userId = User.USER_ID;
+const userPayload = {
+  id: 1,
+  email: "apple@test.com",
+  name: "John Doe",
+};
 
-// const userPayload = {
-//   id: userId,
-//   email: "john.doe@example.com",
-//   name: "John Doe",
-// };
-
-// const userInput = {
-//   email: "test@example.com",
-//   name: "John Doe",
-//   password: "Password123",
-//   passwordConfirmation: "Password123",
-// };
+const userInput = {
+  email: "apple@test.com",
+  name: "John Doe",
+  password: "Password123",
+  passwordConfirmation: "Password123",
+};
 
 describe("user", () => {
   describe("user registration", () => {
     describe("given the username and password are valid", () => {
       it("should return the user payload", async () => {
-        //         const createUserServiceMock = jest
-        //           .spyOn(UserService, "registerUser")
-        //           // @ts-ignore
-        //           .mockReturnValueOnce(userPayload);
-        //         const { statusCode, body } = await supertest(app)
-        //           .post("/api/users")
-        //           .send(userInput);
-        //         expect(statusCode).toBe(200);
-        //         expect(body).toEqual(userPayload);
-        //         expect(createUserServiceMock).toHaveBeenCalledWith(userInput);
+        const createUserServiceMock = jest
+          .spyOn(UserService, "registerUser")
+          // @ts-ignore
+          .mockReturnValueOnce(userPayload);
+        const { statusCode, body } = await supertest(app)
+          .post("/api/users")
+          .send(userInput);
+
+        console.log(statusCode);
+        console.log(body);
+        expect(statusCode).toBe(200);
+
+        expect(body).toEqual(userPayload);
+
+        expect(createUserServiceMock).toHaveBeenCalledWith(
+          userInput.email,
+          userInput.name,
+          userInput.password
+        );
       });
     });
 
     describe("given the passwords do not match", () => {
-      it ("should return a 400", async () => {
-
-      })
-    })
+      it("should return a 400", async () => {});
+    });
 
     describe("given the user service throws", () => {
-      it("should return a 409 error", () => {
-
-      })
-    })
+      it("should return a 409 error", async () => {});
+    });
 
     describe("create user session", () => {
       describe("given the username and password are valid", () => {
-        it("should return a signed accessToken & refresh token", () => {
-          
-        })
-      })
-    })
+        it("should return a signed accessToken & refresh token", async () => {});
+      });
+    });
   });
 });
