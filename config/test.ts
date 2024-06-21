c4751b54ebbccce21b32f18fded7f3944e856006
ivanab <ivana@opticaltone.com>
2024-06-21 18:26:42 +0200

    refactor: removed unused items in config

--- config/test.ts
+++ config/test.ts
@@ -16,10 +16,6 @@ export default {
   signTokenAlgorithm: process.env.SIGN_TOKEN_ALGORITHM,
   accessTokenTtl: process.env.ACCESS_TOKEN_TTL,
   refreshTokenTtl: process.env.REFRESH_TOKEN_TTL,
-  accessTokenPrivateKey: process.env.ACCESS_TOKEN_PRIVATE_KEY,
-  accessTokenPublicKey: process.env.ACCESS_TOKEN_PUBLIC_KEY,
-  refreshTokenPrivateKey: process.env.REFRESH_PRIVATE_KEY,
-  refreshTokenPublicKey: process.env.REFRESH_PUBLIC_KEY,
 
   publicKey: process.env.PUBLIC_KEY,
   privateKey: process.env.PRIVATE_KEY,
