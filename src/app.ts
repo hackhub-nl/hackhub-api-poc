2a6d0fb84fc299085190a1e6aca74a560d6b00c7
ivanab <ivana@opticaltone.com>
2024-06-21 13:04:20 +0200

    feat: added cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 import express from "express";
 import routes from "./routes";
+import cors from "cors";
 import deserializeUser from "./middleware/deserializeUser";
+import config from "config";
 
 const app: express.Application = express();
+const clientUri: string = config.get<string>("clientUri");
+
+const allowedOrigins = [clientUri];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
+app.use(cors(options));
 
 app.use(express.json());
 
