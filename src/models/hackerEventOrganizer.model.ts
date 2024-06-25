684b38f780d360b99a979a355ea31b50d4e8bdf8
ivanab <ivana@opticaltone.com>
2024-06-25 13:12:25 +0200

    feat: created HakerEventOrganizer model

---
+++ src/models/hackerEventOrganizer.model.ts
@@ -0,0 +1,25 @@
+import { Model, Column, DataType, ForeignKey } from "sequelize-typescript";
+import { Organizer } from "./organizer.model";
+import { HackerEvent } from "./hackerEvent.model";
+
+export class HackerEventOrganizer extends Model {
+  public static HACKEREVENT_ORGANIZER_TABLE_NAME =
+    "hackerEventOrganizers" as string;
+  public static HACKEREVENT_ORGANIZER_ID = "id" as string;
+
+  @Column({
+    type: DataType.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    field: HackerEventOrganizer.HACKEREVENT_ORGANIZER_ID,
+  })
+  id!: number;
+
+  @ForeignKey(() => Organizer)
+  @Column
+  organizerId!: number;
+
+  @(ForeignKey!(() => HackerEvent))
+  @Column
+  hackerEventId!: number;
+}
