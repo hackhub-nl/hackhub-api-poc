b4b78632619b28f9dccbe0545b703d5c43457327
ivanab <ivana@opticaltone.com>
2024-07-02 15:37:29 +0200

    test: created tests for organizer

---
+++ src/__tests__/organizer.test.ts
@@ -0,0 +1,24 @@
+import supertest from "supertest";
+import { app } from "../app";
+
+describe("organizer", () => {
+  describe("get organizer route", () => {
+    describe("given the organizer does not exist", () => {
+      it("should return a 404", async () => {
+        const id = "222";
+
+        await supertest(app).get(`/api/organizers/${id}`).expect(404);
+      });
+    });
+  });
+
+  describe("create organizer route", () => {
+    describe("given the user is not logged in", () => {
+      it("should return a 403", async () => {
+        const { statusCode } = await supertest(app).post("/api/organizers");
+
+        expect(statusCode).toBe(403);
+      });
+    });
+  });
+});
