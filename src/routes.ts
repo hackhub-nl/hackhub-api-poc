dedfd54c94682bc08790074814172cbeeac67264
ivanab <ivana@opticaltone.com>
2024-07-02 18:19:31 +0200

    feat: separated admin routes

--- src/routes.ts
+++ src/routes.ts
@@ -41,7 +41,7 @@ import {
   getAllOrganizersHandler,
   getOrganizerHandler,
   updateOrganizerHandler,
-} from "./controllers/organizer.cotroler";
+} from "./controllers/organizer.controller";
 import {
   createOrganizerSchema,
   deleteOrganizerSchema,
@@ -49,26 +49,111 @@ import {
   updateOrganizerSchema,
 } from "./schema/organizer.schema";
 
-// curl http://localhost:port/healthcheck
-
 function routes(app: express.Application) {
-  app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
+  // app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
 
+  // Admin routes:
+  // Admin: users
   app.post(
-    "/api/users",
+    "/api/admin/users",
     validateResource(registerUserSchema),
     registerUserHandler
   );
 
+  // Admin: sessions
+  app.get("/api/admin/sessions", requireUser, getUserSessionsHandler);
+
   app.post(
-    "/api/sessions",
+    "/api/admin/sessions",
     validateResource(loginSessionSchema),
     loginUserSessionHandler
   );
 
-  app.get("/api/sessions", requireUser, getUserSessionsHandler);
-  app.delete("/api/sessions", requireUser, deleteSessionHandler);
+  app.delete("/api/admin/sessions", requireUser, deleteSessionHandler);
 
+  // Admin: hackerspaces
+  app.get("/api/admin/hackerspaces", getAllHackerspacesHandler);
+
+  app.get(
+    "/api/admin/hackerspaces/:id",
+    validateResource(getHackerspaceSchema),
+    getHackerspaceHandler
+  );
+
+  app.post(
+    "/api/admin/hackerspaces",
+    [requireUser, validateResource(createHackerspaceSchema)],
+    createHackerspaceHandler
+  );
+
+  app.put(
+    "/api/admin/hackerspaces/:id",
+    [requireUser, validateResource(updateHackerspaceSchema)],
+    updateHackerspaceHandler
+  );
+
+  app.delete(
+    "/api/admin/hackerspaces/:id",
+    [requireUser, validateResource(deleteHackerspaceSchema)],
+    deleteHackerspaceHandler
+  );
+
+  // Admin: hackerEvents
+  app.get("/api/admin/hackerevents", getAllHackerEventsHandler);
+
+  app.get(
+    "/api/admin/hackerevents/:id",
+    validateResource(getHackerEventSchema),
+    getHackerEventHandler
+  );
+
+  app.post(
+    "/api/admin/hackerevents",
+    [requireUser, validateResource(createHackerEventSchema)],
+    createHackerEventHandler
+  );
+
+  app.put(
+    "/api/admin/hackerevents/:id",
+    [requireUser, validateResource(updateHackerEventSchema)],
+    updateHackerEventHandler
+  );
+
+  app.delete(
+    "/api/admin/hackerevents/:id",
+    [requireUser, validateResource(deleteHackerEventSchema)],
+    deleteHackerEventHandler
+  );
+
+  // Admin: organizers
+  app.get("/api/admin/organizers", getAllOrganizersHandler);
+
+  app.get(
+    "/api/admin/organizers/:id",
+    validateResource(getOrganizerSchema),
+    getOrganizerHandler
+  );
+
+  app.post(
+    "/api/admin/organizers",
+    [requireUser, validateResource(createOrganizerSchema)],
+    createOrganizerHandler
+  );
+
+  app.put(
+    "/api/admin/organizers/:id",
+    [requireUser, validateResource(updateOrganizerSchema)],
+    updateOrganizerHandler
+  );
+
+  app.delete(
+    "/api/admin/organizers/:id",
+    [requireUser, validateResource(deleteOrganizerSchema)],
+    deleteOrganizerHandler
+  );
+
+  // Website visitor routes:
+  // Website visitor: hackerspaces
   app.get("/api/hackerspaces", getAllHackerspacesHandler);
 
   app.get(
@@ -77,24 +162,7 @@ function routes(app: express.Application) {
     getHackerspaceHandler
   );
 
-  app.post(
-    "/api/hackerspaces",
-    [requireUser, validateResource(createHackerspaceSchema)],
-    createHackerspaceHandler
-  );
-
-  app.put(
-    "/api/hackerspaces/:id",
-    [requireUser, validateResource(updateHackerspaceSchema)],
-    updateHackerspaceHandler
-  );
-
-  app.delete(
-    "/api/hackerspaces/:id",
-    [requireUser, validateResource(deleteHackerspaceSchema)],
-    deleteHackerspaceHandler
-  );
-
+  // Website visitor: hackerEvents
   app.get("/api/hackerevents", getAllHackerEventsHandler);
 
   app.get(
@@ -103,48 +171,13 @@ function routes(app: express.Application) {
     getHackerEventHandler
   );
 
-  app.post(
-    "/api/hackerevents",
-    [requireUser, validateResource(createHackerEventSchema)],
-    createHackerEventHandler
-  );
-
-  app.put(
-    "/api/hackerevents/:id",
-    [requireUser, validateResource(updateHackerEventSchema)],
-    updateHackerEventHandler
-  );
-
-  app.delete(
-    "/api/hackerevents/:id",
-    [requireUser, validateResource(deleteHackerEventSchema)],
-    deleteHackerEventHandler
-  );
-
+  // Website visitor: organizers
   app.get("/api/organizers", getAllOrganizersHandler);
 
   app.get(
     "/api/organizers/:id",
     validateResource(getOrganizerSchema),
     getOrganizerHandler
-  );
-
-  app.post(
-    "/api/organizers",
-    [requireUser, validateResource(createOrganizerSchema)],
-    createOrganizerHandler
-  );
-
-  app.put(
-    "/api/organizers/:id",
-    [requireUser, validateResource(updateOrganizerSchema)],
-    updateOrganizerHandler
-  );
-
-  app.delete(
-    "/api/organizers/:id",
-    [requireUser, validateResource(deleteOrganizerSchema)],
-    deleteOrganizerHandler
   );
 }
 
